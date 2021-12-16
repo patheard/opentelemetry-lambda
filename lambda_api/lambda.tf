006e48aa2d332940e81267d443545b847c57a872
Pat Heard <patrick.heard@cds-snc.ca>
2021-12-16 18:06:04 +0000

    feat: create a null_resource lambda builder

--- lambda_api/lambda.tf
+++ lambda_api/lambda.tf
@@ -7,17 +7,21 @@ data "archive_file" "lambda_zip" {
   source_dir  = "src/dist"
   output_path = "/tmp/lambda.zip"
   type        = "zip"
+
+  depends_on = [
+    null_resource.lambda_build
+  ]
 }
 
 resource "aws_lambda_function" "api_lambda" {
-  filename      = "/tmp/lambda.zip"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
   function_name = "lambda_api"
   handler       = "lambda.handler"
   runtime       = "python3.8"
   timeout       = 10
   role          = aws_iam_role.lambda.arn
-
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   layers = [
     local.otel_layer_arn
@@ -39,6 +43,17 @@ resource "aws_lambda_function" "api_lambda" {
   }
 }
 
+resource "null_resource" "lambda_build" {
+  triggers = {
+    handler      = base64sha256(file("src/lambda.py"))
+    requirements = base64sha256(file("src/requirements.txt"))
+    build        = base64sha256(file("src/build.sh"))
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/src/build.sh"
+  }
+}
 
 # Allow the API gateway to invoke this lambda function
 resource "aws_lambda_permission" "api_lambda_permission" {
