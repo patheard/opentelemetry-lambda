231755bdf1ca60c5e734d5850c1e7f84db9fb101
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-12-17 19:01:46 -0500

    feat: example of fastapi instrumented not working with mangum

---
+++ lambda_api_fastapi_instrumented/src/lambda.py
@@ -0,0 +1,36 @@
+"""
+Main API handler that defines all routes.
+"""
+
+import boto3
+import os
+
+from fastapi import FastAPI
+from mangum import Mangum
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+app = FastAPI(
+    title="AWS + FastAPI",
+    description="AWS API Gateway, Lambdas and FastAPI (oh my)",
+    root_path="/dev"
+)
+
+@app.get("/hello")
+def hello():
+    "Hello path request"
+    return {"Hello": "World"}
+
+@app.get("/list")
+def hello():
+    client = boto3.client("s3")
+    client.list_buckets()
+    
+    client = boto3.client("ec2")
+    client.describe_instances()
+
+    return {"Region ": os.environ['AWS_REGION']}  
+
+
+FastAPIInstrumentor.instrument_app(app)
+# Mangum allows us to use Lambdas to process requests
+handler = Mangum(app=app)
