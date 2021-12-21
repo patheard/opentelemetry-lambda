1da22868ccfb2414089ae9e22f2a111f8d5559a0
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-12-21 10:23:34 -0500

    feat: docker lambda working with otel

--- lambda_docker/lambda.py
+++ lambda_docker/lambda.py
@@ -1,22 +1,36 @@
-#! /usr/bin/python3
+"""
+Main API handler that defines all routes.
+"""
 
 import boto3
-import json
 import os
 
-def handler(event, context):
+from fastapi import FastAPI
+from mangum import Mangum
+
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+app = FastAPI(
+    title="AWS + FastAPI",
+    description="AWS API Gateway, Lambdas and FastAPI (oh my)",
+    root_path="/dev"
+)
+
+@app.get("/hello")
+def hello():
+    "Hello path request"
+    return {"Hello": "World"}
+
+@app.get("/list")
+def hello():
     client = boto3.client("s3")
     client.list_buckets()
     
     client = boto3.client("ec2")
     client.describe_instances()
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "Region ": os.environ['AWS_REGION']
-        })
-    }
+    return {"Region ": os.environ['AWS_REGION']}  
+
+FastAPIInstrumentor.instrument_app(app)
+# Mangum allows us to use Lambdas to process requests
+handler = Mangum(app=app)
