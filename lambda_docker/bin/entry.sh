4a8d3cb351f18c13264b7df3b97504ac816345f7
Mohamed Nur <mohamed.nur@cds-snc.ca>
2021-12-22 14:21:47 -0500

    feat: working example of docker lambda otel instrumentation

--- lambda_docker/bin/entry.sh
+++ lambda_docker/bin/entry.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
 if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
     echo "Running aws-lambda-rie"
     exec find . -name "*.py" | entr -r /usr/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric "$1"
 else
-    exec opentelemetry-instrument /usr/local/bin/python -m awslambdaric "$1"
+    if [ -z "$AWS_LAMBDA_EXEC_WRAPPER" ]; then
+        exec /usr/local/bin/python -m awslambdaric "$1"
+    else
+        exec -- "$AWS_LAMBDA_EXEC_WRAPPER" /usr/local/bin/python -m awslambdaric "$1"
+    fi
 fi
